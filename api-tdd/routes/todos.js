c031b5ae289c4b9ad211a3834ce8cbba07a31c63
Baney Acosta <baneymelo@gmail.com>
2021-06-03 00:05:03 -0500

    secod test GET /todos/id passed

--- api-tdd/routes/todos.js
+++ api-tdd/routes/todos.js
@@ -1,15 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
-const todos = [{
-    id: 1,
-    name: 'Do something',
-    completed: false    
-}]
+const todos = [
+    {
+        id: 1,
+        name: 'Do something',
+        completed: false,
+    },
+    {
+        id: 2,
+        name: 'Do task',
+        completed: true    
+    }
+]
 
 // /todos/
 router.get('/', function(req, res, next) {
   res.json(todos);
+});
+
+router.get('/:id', function(req, res, next) {
+  const { id } = req.body;
+  const found = todos.find(todo => todo.id === id);
+  res.json(found);
 });
 
 module.exports = router;
